7e4194ba41c6ae16a5c4f6b8664fce6e36f3a4f5
patr-lew <patryk.a.lewandowski@gmail.com>
2021-06-22 00:57:12 +0200

    created services

---
+++ src/main/java/pl/coderslab/charity/service/InstitutionService.java
@@ -0,0 +1,12 @@
+package pl.coderslab.charity.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.coderslab.charity.repository.InstitutionRepository;
+
+@Service
+@RequiredArgsConstructor
+public class InstitutionService {
+
+    private final InstitutionRepository institutionRepository;
+}
