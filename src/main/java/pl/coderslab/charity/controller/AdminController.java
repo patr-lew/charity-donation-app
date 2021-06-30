afb4f05d6d2877921ca1268d2d00c0a4fdc4a91d
patr-lew <patryk.a.lewandowski@gmail.com>
2021-06-30 09:29:03 +0200

    admin view set

---
+++ src/main/java/pl/coderslab/charity/controller/AdminController.java
@@ -0,0 +1,19 @@
+package pl.coderslab.charity.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/dashboard")
+    public String getDashboard(Model model) {
+        return "admin/index";
+    }
+
+}
--- src/main/java/pl/coderslab/charity/security/WebSecurityConfig.java
+++ src/main/java/pl/coderslab/charity/security/WebSecurityConfig.java
@@ -1,15 +1,12 @@
 package pl.coderslab.charity.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,16 +14,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                    .antMatchers("/donations/**").hasAnyRole("USER", "ADMIN")
                     .antMatchers("/**").permitAll()
-                    .antMatchers("/donations/**").hasRole("USER")
                 .and()
-                .formLogin()
+                    .formLogin()
                     .loginPage("/login")
                     .usernameParameter("email")
                     .failureUrl("/login?error=true")
                     .permitAll()
                 .and()
-                .logout().logoutSuccessUrl("/")
+                    .logout()
+                    .logoutSuccessUrl("/")
                     .permitAll()
                 .and().httpBasic();
     }
